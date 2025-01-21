88c7cc66df48137c920fedc0f303847d09ac1369
asraful0106 <asrafulapf@gmail.com>
2025-01-21 20:27:32 +0600

    Footer is Done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router";
 import NavBar from "../sharedComponent/NavBar";
+import Footer from "../sharedComponent/Footer";
 
 const MainLayout = () => {
     return (
         <>
-            <NavBar />
-            <Outlet />
+            <div className="flex flex-col min-h-screen">
+                <NavBar />
+                {/* Main Content */}
+                <main className="flex-grow">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
         </>
     );
 };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import MainLayout from '../layout/MainLayout';
+import HomePage from '../pages/HomePage';
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Routes>
+            {/* <Routes>
                 <Route path="/" element={<MainLayout />} />
+            </Routes> */}
+            <Routes>
+                <Route path="/" element={<MainLayout />} >
+                    <Route path="/" element={<HomePage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
